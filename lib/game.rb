2da8e98d453fd543a36bd4c206963c1c2ddefca2
Markus Krogemann <markus@krogemann.de>
2012-12-29 17:14:27 +0100

    add example for a still life (beehive)

--- lib/game.rb
+++ lib/game.rb
@@ -1,65 +1,67 @@
 class Game
-       
+
+  attr_reader :grid
+
   def initialize(columns, rows)
     raise "Game must be at least 3 by 3 cells in size" unless columns > 2 && rows > 2
     @columns = columns
     @rows = rows
     @grid = init_grid
     @rules = Rules.new
-  end    
-        
-  def init_grid 
+  end
+
+  def init_grid
     new_grid = {}
     Range.new(1, @columns).to_a.each do |x_|
       Range.new(1, @rows).to_a.each do |y_|
         new_grid[key(x_, y_)] = false
-      end      
+      end
     end
     new_grid
   end
-  
-  def evolve(current_grid = nil)               
-    next_grid = init_grid              
+
+  def evolve(current_grid = nil)
+    next_grid = init_grid
     @grid = current_grid if current_grid
     @grid.each_key do |key|
       x, y = coordinates(key)
       next_grid[key] = @rules.evaluate(living_neighbors(x, y), alive?(key))
-    end                                                                                 
+    end
     next_grid
   end
-  
+
   def set_alive(x, y)
     @grid[key(x, y)] = true
   end
-  
-  def alive?(key)  
+
+  def alive?(key)
     @grid[key] == true
-  end  
-  
+  end
+
   def living_neighbors(x, y)
-    living = neighboring_keys(x,y).inject(0) do |result, key| 
+    living = neighboring_keys(x,y).inject(0) do |result, key|
       result += 1 if alive?(key)
       result
     end
-  end                 
-  
+  end
+
   def neighboring_keys(x, y)
     neighbor_keys = []
     ((x-1)..(x+1)).to_a.each do |x_|
       ((y-1)..(y+1)).to_a.each do |y_|
         neighbor_keys << key(x_,y_) unless x == x_ && y == y_
       end
-    end                             
+    end
     neighbor_keys
   end
-  
+
   def key(x, y)
     "#{x}_#{y}"
-  end  
-  
+  end
+
   def coordinates(key)
     split = key.split('_')
     [split[0].to_i, split[1].to_i]
   end
-  
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 require 'game'
+require 'game_spec_helper'
 
 describe Game do
-       
+
   context '#initialize' do
     it "a Game must have a minimum dimension of 3 by 3 cells" do
       expect {
@@ -12,92 +13,116 @@ describe Game do
         Game.new(3,2)
       }.to raise_error
     end
-  end  
-  
+  end
+
   context '#alive?' do
-    it "a new Game returns dead cell for any coordinates within its grid boundaries" do 
+    it "a new Game returns dead cell for any coordinates within its grid boundaries" do
       game = Game.new(5,5)
-  
-      game.alive?(game.key(rand(5),rand(5))).should be_false    
+
+      game.alive?(game.key(rand(5),rand(5))).should be_false
     end
-  
+
     it "a new Game accepts and returns a living cell at any coordinates within its grid boundaries" do
-      x,y = rand(5), rand(5) 
+      x,y = rand(5), rand(5)
       game = Game.new(5,5)
-  
+
       game.set_alive(x,y)
-  
+
       game.alive?(game.key(x,y)).should be_true
-    end                 
+    end
   end
-     
-  context '#neighboring_keys' do                                          
+
+  context '#neighboring_keys' do
     it "any Game returns 8 neighboring cell coordinates for any given cell coordinate" do
-      x,y = rand(5), rand(5)         
+      x,y = rand(5), rand(5)
       game = Game.new(5,5)
-    
+
       game.neighboring_keys(x,y).size.should == 8
-    end                           
+    end
   end
-  
+
   context '#living_neighbors' do
     it "a new Game with one living cell returns zero living cells around that one living cell" do
-      x,y = rand(5), rand(5)         
-    
+      x,y = rand(5), rand(5)
+
       game = Game.new(5,5)
       game.set_alive(x,y)
-    
-      game.living_neighbors(x,y).should == 0                        
-    end 
-  
+
+      game.living_neighbors(x,y).should == 0
+    end
+
     it "a new Game with two adjacent living cells returns a count of one living cell around the initial living cell" do
       game = Game.new(5,5)
-    
+
       game.set_alive(2,3)
       game.set_alive(2,4)
-    
+
       game.living_neighbors(2,3).should == 1
     end
-  
+
     it "a new Game with three adjacent living cells returns a count of two living cells around the initial living cell" do
       game = Game.new(5,5)
-    
+
       game.set_alive(2,3)
       game.set_alive(2,4)
       game.set_alive(3,3)
-    
+
       game.living_neighbors(2,3).should == 2
-    end   
+    end
   end
-  
+
   context '#coordinates' do
     it "splits a key given as String into an array of integer coordinates" do
       game = Game.new(5,5)
-    
-      game.coordinates('1_3').should == [1,3]    
-    end      
+
+      game.coordinates('1_3').should == [1,3]
+    end
   end
-  
+
   context '#evolve' do
     it "compultes next two generations for an oscillator" do
       game = Game.new(5,5)
-    
+
       game.set_alive(3,2)
       game.set_alive(3,3)
       game.set_alive(3,4)
-    
+
       next_grid = game.evolve
-    
+
       next_grid[game.key(2,3)].should be_true
       next_grid[game.key(3,3)].should be_true
-      next_grid[game.key(4,3)].should be_true   
-    
+      next_grid[game.key(4,3)].should be_true
+
       next_next_grid = game.evolve(next_grid)
-    
+
       next_next_grid[game.key(3,2)].should be_true
       next_next_grid[game.key(3,3)].should be_true
-      next_next_grid[game.key(3,4)].should be_true    
-    end   
-  end  
-    
+      next_next_grid[game.key(3,4)].should be_true
+    end
+
+    it "computes a behive which is one example for a still life" do
+      game = Game.new(6,5)
+
+      game.set_alive(2,3)
+      game.set_alive(3,2)
+      game.set_alive(3,4)
+      game.set_alive(4,2)
+      game.set_alive(4,4)
+      game.set_alive(5,3)
+
+      next_grid = game.evolve
+
+      next_grid[game.key(2,3)].should be_true
+      next_grid[game.key(3,2)].should be_true
+      next_grid[game.key(3,4)].should be_true
+      next_grid[game.key(4,2)].should be_true
+      next_grid[game.key(4,4)].should be_true
+      next_grid[game.key(5,3)].should be_true
+
+      GameSpecHelper.count_cells_alive(game.grid).should == 6
+
+    end
+
+  end
+
 end
