2eb6a3c47421755122dbb127bf66014d8b41c967
Markus Krogemann <markus@krogemann.de>
2013-01-08 03:07:48 +0100

    grrmh, misreading x and y is bad.

--- lib/game_printer.rb
+++ lib/game_printer.rb
@@ -1,8 +1,8 @@
 class GamePrinter
 
   def self.print(game, io = STDOUT)
-    Range.new(1, game.columns).to_a.each do |y|
-      Range.new(1, game.rows).to_a.each do |x|
+    Range.new(1, game.columns).to_a.each do |x|
+      Range.new(1, game.rows).to_a.each do |y|
         printf(io, game.grid[game.key(x,y)] == true ? 'o' : ' ')
       end
       printf(io, "\n")
--- spec/game_printer_spec.rb
+++ spec/game_printer_spec.rb
@@ -9,7 +9,7 @@ describe GamePrinter do
     it 'produces expected output in given io object (String)' do
       game = Game.new(9,7, ["2_4", "3_5", "1_3", "5_5"])
       result = GamePrinter.print(game, StringIO.new).string
-      result.should == "       \n       \no      \n o     \n  o o  \n       \n       \n       \n       \n"
+      result.should == "  o    \n   o   \n    o  \n       \n    o  \n       \n       \n       \n       \n"
     end
   end
 
