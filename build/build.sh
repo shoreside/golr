9d6c7438e1127a2d9d366386b1aa21e96fa8dfc6
Markus Krogemann <markus@krogemann.de>
2013-01-12 23:51:16 +0100

    incorporate cucumber run and gitignore its results

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -l
 
+# this script is here to be used by Go and similar CI/CD products. It may at some point be replaced by a Rakefile.
+
 rvm use 1.9.3@golr --create
 bundle
 [ -d rspec ] && rm -rf rspec
-rspec --out rspec/rspec.xml --format html --out rspec/rspec.html
+bundle exec rspec --out rspec/rspec.xml --format html --out rspec/rspec.html
+
+[ -d cukeresults ] && rm -rf cukeresults
+bundle exec cucumber --format junit --out cukeresults --no-source
